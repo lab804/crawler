91bb22b3b7b987b36389724bdc84d3a487095fcd
barbara <barbara@lab804.com.br>
2017-02-21 10:04:32 -0300

    read csv

--- crawler/read.py
+++
@@ -1,9 +0,0 @@
-import csv, sys
-nome_ficheiro = 'ficheiro.csv'
-with open(nome_ficheiro, 'rb') as ficheiro:
-    reader = csv.reader(ficheiro)
-    try:
-        for linha in reader:
-            print linha
-    except csv.Error as e:
-        sys.exit('ficheiro %s, linha %d: %s' % (nome_ficheiro, reader.line_num, e))
---
+++ read.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import csv
+
+data = []
+
+keys = open("2629_SP_2017_2.csv", "r").readline().strip().split(";")
+
+with open('2629_SP_2017_2.csv', 'rb') as csvfile:
+     for book in csvfile.readlines(): 
+         b = book.strip().replace(b"\t", b"")
+         b_as_list = b.split(b";")
+         book_dict = {}
+         for valor in b_as_list:
+             if valor:
+                 book_dict[keys[b_as_list.index(valor)]] = valor
+         data.append(book_dict)
+
+del data[0]
+print(data)
