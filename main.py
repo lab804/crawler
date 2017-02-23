20b6c4bb0675189a0c57e93a2d60287c8d936b5c
barbara <barbara@lab804.com.br>
2017-02-23 16:24:31 -0300

    banco dads

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import requests
+import re
 
 # armazenamos a sessao da requisicao (cookies, sessions, headers...)
 session = requests.Session()
+
+def start(url):
+    response = session.get(url)
+    cities_state = re.findall('\(\"(\w{2})\"\,\"(.*?)\"\,\"(\d+)\"', response.text)
+    for city in cities_state:
+        print(city)
+
 
 def captcha(url, fname):
     """ download captcha """
@@ -17,25 +25,27 @@ def captcha(url, fname):
 def main():
     """ funcao principal """
     urlsite = 'http://150.163.255.234/salvar/mapainterativo/downpluv.php'
-    path = urlsite+'?idUF=SP&idCidade=ITAPETININGA&edMes=2&edAno=2017&edNome=Barbara&edEmail=barbara%40lab804.com.br&palavra={palavra}'
-    urlimagem = 'http://150.163.255.234/salvar/mapainterativo/securimage/securimage_show.php'
-    fname = 'qtest.jpg'
-    try:
-        iscaptcha = captcha(urlimagem, fname)
-        if iscaptcha:
-            palavra = input("Me fale as letras senhorita? ")
-            finalurl = path.format(palavra=palavra)
-            response = session.get(finalurl)
-            if response.status_code == 200:
-                print(response.content)
-            else:
-                print("Deu pau, tente novamente...")
-        else:
-            print("ops nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
-    except requests.exceptions.HTTPError as e:
-        print("%s '%s'" % (e, url))
+    start(urlsite)
+    # path = urlsite+'?idUF=SP&idCidade=ITAPETININGA&edMes=2&edAno=2017&edNome=Barbara&edEmail=barbara%40lab804.com.br&palavra={palavra}'
+    # urlimagem = 'http://150.163.255.234/salvar/mapainterativo/securimage/securimage_show.php'
+    # fname = 'qtest.jpg'
+    # try:
+    #     iscaptcha = captcha(urlimagem, fname)
+    #     if iscaptcha:
+    #         palavra = input("Me fale as letras senhorita? ")
+    #         finalurl = path.format(palavra=palavra)
+    #         response = session.get(finalurl)
+    #         if response.status_code == 200:
+    #             print(response.content)
+    #         else:
+    #             print("Deu pau, tente novamente...")
+    #     else:
+    #         print("ops nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
+    # except requests.exceptions.HTTPError as e:
+    #     print("%s '%s'" % (e, url))
 
 if __name__ == "__main__":
     print("CTRL+C para sair")
     while True:
         main()
+        break
