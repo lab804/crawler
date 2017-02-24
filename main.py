20074bdc4c9e2a067bff12edc3109ebe0a61915e
barbara <barbara@lab804.com.br>
2017-02-24 14:55:43 -0300

    salvando imagem jpg e o codigo como nome / salvando tambem os codigos em uma arquivo unitario csv

--- main.py
+++ main.py
@@ -2,18 +2,18 @@
 # -*- coding: iso-8859-1 -*-
 import requests
 import re
-import shutil
 import os
 
 # armazenamos a sessao da requisicao (cookies, sessions, headers...)
 session = requests.Session()
 
 def start():
+    """ start captura dados url"""
     urlsite = 'http://150.163.255.234/salvar/mapainterativo/downpluv.php'
     response = session.get(urlsite)
     cities_state = re.findall('\(\"(\w{2})\"\,\"(.*?)\"\,\"(\d+)\"', response.text)
     for cities in cities_state:
-        urlcomplete(urlsite, cities[0], cities[1])
+        url_complete(urlsite, cities[0], cities[1])
 
 def captcha(url):
     """ download captcha """
@@ -27,8 +27,8 @@ def captcha(url):
             print("erro ao salvar o captcha: %s" % e)
             return False
 
-def urlcomplete(urlsite, state, cities):
-
+def url_complete(urlsite, state, cities):
+    """ trata os dados coletados e mundo a url completa"""
     urlimagem = 'http://150.163.255.234/salvar/mapainterativo/securimage/securimage_show.php'
     path = formating(urlsite, state, cities)
     print(path)
@@ -39,8 +39,9 @@ def urlcomplete(urlsite, state, cities):
             finalurl = path+cap
             response = session.get(finalurl)
             if response.status_code == 200:
+                write_file(cap)
+                rename_file(cap)
                 print(response.content)
-                os.rename('captcha.jpg', cap)
             else:
                 print("Deu pau, tente novamente...")
         else:
@@ -49,7 +50,7 @@ def urlcomplete(urlsite, state, cities):
         print("%s '%s'" % (e, url))
 
 def formating(urlsite, state, c):
-
+    """formata path a partir dos dados coletaods"""
     cities = c.replace(" ", "+")
     print(cities)
     base = '?idUF={est}&idCidade={city}&edMes=2&edAno=2017&edNome=Barbara&edEmail=barbara%40lab804.com.br&palavra='
@@ -57,8 +58,23 @@ def formating(urlsite, state, c):
     urlpath = urlsite+baseurl
     return urlpath
 
+def rename_file(cap_code):
+    """renomeia arquivo captcha com nome-codigo"""
+    namecap = 'cap/'+cap_code+'.jpg'
+    os.rename('captcha.jpg', namecap)
+
+def write_file(cap_code):
+    """registra os codigos do captcha atual ao final do arquivo csv"""
+    try:
+        arq = open('captcha.csv','a+')
+        arq.writelines('\n'+cap_code)
+        print("Registro gravado com sucesso")
+        arq.close()
+    except IOError:
+        print("Erro ao abrir o arquivo!")
+
 if __name__ == "__main__":
     print("CTRL+C para sair")
+
     while True:
         start()
-        break
