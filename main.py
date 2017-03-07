7930776011d340400e544941de736d958312675e
barbara <barbara@lab804.com.br>
2017-03-07 16:42:01 -0300

    funcionando perfeitamente - estruturacao em class

--- main.py
+++ main.py
@@ -4,74 +4,79 @@ import requests
 import re
 import os
 
-# armazenamos a sessao da requisicao (cookies, sessions, headers...)
-session = requests.Session()
+class RequestFile(object):
 
-def start():
-    """ Start na captura de dados e captcha pela url"""
-    urlsite = 'http://150.163.255.234/salvar/mapainterativo/downpluv.php'
-    response = session.get(urlsite)
-    cities_state = re.findall('\(\"(\w{2})\"\,\"(.*?)\"\,\"(\d+)\"', response.text)
-    for cities in cities_state:
-        url_complete(urlsite, cities[0], cities[1])
+    def __init__(self):
+        self.session = requests.Session()
+        self.urlsite = 'http://150.163.255.234/salvar/mapainterativo/downpluv.php'
 
-def captcha(url):
-    """ Realiza download da imagem do captcha"""
-    fname = 'captcha.jpg'
-    resp = session.get(url)
-    with open(fname, 'wb') as f:
+    def start(self):
+        """ Start na captura de dados e captcha pela url"""
+        response = self.session.get(self.urlsite)
+        cities_state = re.findall('\(\"(\w{2})\"\,\"(.*?)\"\,\"(\d+)\"', response.text)
+        for cities in cities_state:
+            self.url_complete(self.urlsite, cities[0], cities[1])
+
+    def captcha(self, url):
+        """ Realiza download da imagem do captcha"""
+        fname = 'captcha.jpg'
+        resp = self.session.get(url)
+        with open(fname, 'wb') as f:
+            try:
+                f.write(resp.content)
+                return True
+            except Exception as e:
+                print("Erro ao salvar o captcha: %s" % e)
+                return False
+
+    def url_complete(self, urlsite, state, cities):
+        """ Completa a url com estado, cidade e o código do captcha.
+        Verifica o status code da requisição da página; Caso tenha sucesso,
+        salva imagem do captcha com o seu código de nome na pasta '/cap' """
+        urlimagem = 'http://150.163.255.234/salvar/mapainterativo/securimage/securimage_show.php'
+        path = self.formatting(urlsite, state, cities)
+        print(path)
         try:
-            f.write(resp.content)
-            return True
-        except Exception as e:
-            print("Erro ao salvar o captcha: %s" % e)
-            return False
-
-def url_complete(urlsite, state, cities):
-    """ Completa a url com estado, cidade e o código do captcha.
-    Verifica o status code da requisição da página; Caso tenha sucesso,
-    salva imagem do captcha com o seu código de nome na pasta '/cap' """
-    urlimagem = 'http://150.163.255.234/salvar/mapainterativo/securimage/securimage_show.php'
-    path = formatting(urlsite, state, cities)
-    print(path)
-    try:
-        iscaptcha = captcha(urlimagem)
-        if iscaptcha:
-            cap = input("Me fale as letras senhorita? ")
-            finalurl = path+cap
-            response = session.get(finalurl)
-            if response.status_code == 200:
-                print(response.content)
-                write_file(cap)
-                namecap = 'cap/'+cap+'.jpg'
-                os.rename('captcha.jpg', namecap)
+            iscaptcha = self.captcha(urlimagem)
+            if iscaptcha:
+                cap = input("Me fale as letras senhorita? ")
+                finalurl = path+cap
+                response = self.session.get(finalurl)
+                if response.status_code == 200:
+                    print(response.content)
+                    self.write_file(cap)
+                    namecap = 'cap/'+cap+'.jpg'
+                    os.rename('captcha.jpg', namecap)
+                else:
+                    print("Deu pau, tente novamente...")
             else:
-                print("Deu pau, tente novamente...")
-        else:
-            print("Ops nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
-    except requests.exceptions.HTTPError as e:
-        print("%s '%s'" % (e, url))
+                print("Ops nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
+        except requests.exceptions.HTTPError as e:
+            print("%s '%s'" % (e, url))
 
-def formatting(urlsite, state, c):
-    """Formada path com os dados de cidade e estado de acordo com a lista gerado por Start()"""
-    cities = c.replace(" ", "+")
-    print(cities)
-    base = '?idUF={est}&idCidade={city}&edMes=1&edAno=2017&edNome=Barbara&edEmail=barbara%40lab804.com.br&palavra='
-    baseurl = base.format(est=state, city=cities)
-    urlpath = urlsite+baseurl
-    return urlpath
+    @staticmethod
+    def formatting(urlsite, state, c):
+        """Formada path com os dados de cidade e estado de acordo com a lista gerado por Start()"""
+        cities = c.replace(" ", "+")
+        print(cities)
+        base = '?idUF={est}&idCidade={city}&edMes=1&edAno=2017&edNome=Barbara&edEmail=barbara%40lab804.com.br&palavra='
+        baseurl = base.format(est=state, city=cities)
+        urlpath = urlsite+baseurl
+        return urlpath
 
-def write_file(cap_code):
-    """Salva os codigos dos captchas ao final do arquivo captcha.csv"""
-    try:
-        arq = open('captcha.csv','a+')
-        arq.writelines(cap_code+'\n')
-        print("Registro gravado com sucesso")
-    except IOError:
-        print("Erro ao abrir o arquivo!")
+    @staticmethod
+    def write_file(cap_code):
+        """Salva os codigos dos captchas ao final do arquivo captcha.csv"""
+        try:
+            arq = open('captcha.csv','a+')
+            arq.writelines(cap_code+'\n')
+            print("Registro gravado com sucesso")
+        except IOError:
+            print("Erro ao abrir o arquivo!")
 
 if __name__ == "__main__":
     print("CTRL+C para sair")
 
     while True:
-        start()
+        init = RequestFile()
+        init.start()
--- read.py
+++ read.py
@@ -3,44 +3,76 @@
 import csv
 import os
 
-def open_file(filename):
-    """Responsavel por abrir o arquivo e verificar se o arquivo existe. Caso exista, verifica se nao esta vazio"""
-    if os.path.isfile(filename):
-        if os.path.getsize(filename) > 0:
-            read_file(filename)
+class ReadFile(object):
+
+    def __init__(self, filename, city=None, state=None, date_hour=None):
+        self.filename = filename
+        self.city = city
+        self.state = state
+        self.date_hour = date_hour
+
+        def __exit__(self):
+            self.filename.close()
+
+    def start_file(self):
+        if os.path.isfile(self.filename):
+            if os.path.getsize(self.filename) > 0:
+                self.open_file()
+            else:
+                print("Erro: Arquivo vazio")
         else:
-            print("Erro: Arquivo vazio")
-    else:
-        print("Erro: Arquivo nao existe")
+            print("Erro: Arquivo nao existe")
 
+    def read_full_file(self, keys):
+        """Responsavel por realizar a leitura dos dados no arquivo."""
+        data_file = []
+        with open(self.filename, 'rb') as csvfile:
+            for line in csvfile.readlines():
+                b = line.strip().replace(b"\t", b"")
+                b_as_list = b.split(b";")
+                dic = {}
+                for data in b_as_list:
+                    if data:
+                        dic[keys[b_as_list.index(data)]] = data
+                data_file.append(dic)
+        del data_file[0]
+        print(data_file)
 
+    # def read_file (self, keys):
+    #
+    #     data_file = []
+    #     with open(self.filename, 'rb') as csvfile:
+    #         for line in csvfile.readlines():
+    #             b = line.strip().replace(b"\t", b"")
+    #             b_as_list = b.split(b";")
+    #             dic = {}
+    #             for data!=self.date_hour in b_as_list:
+    #                 if data=self.date_hour:
+    #                     dic[keys[b_as_list.index(data)]] = data
+    #
+    #         data_file.append(dic)
+    #     del data_file[0]
+    #     print(data_file)
 
-def read_file(filename):
-    """Responsavel por realizar a leitura dos dados no arquivo. Verifica se esta corrompido ou caso esteja faltando dados"""
-    data = []
-    keys = open(filename, "r").readline().strip().split(";")
-    if keys[0] == 'municipio':
-        with open('2629_SP_2017_2.csv', 'rb') as csvfile:
-             for book in csvfile.readlines():
-                 if book:
-                     print("Arquivo nao contem dados")
-                 else:
-                     b = book.strip().replace(b"\t", b"")
-                     b_as_list = b.split(b";")
-                     book_dict = {}
-                     for valor in b_as_list:
-                         if valor:
-                             book_dict[keys[b_as_list.index(valor)]] = valor
-                     data.append(book_dict)
-             del data[0]
-             print(data)
-    else:
-        print("Arquivo corrompido ou já baixado")
+    def open_file(self):
+
+        keys = open(filename, "r").readline().strip().split(";" or "\n")
+        if keys[0] == 'municipio':
+            if self.date_hour==None:
+                self.read_full_file(keys)
+            else:
+                self.read_file(keys)
+        else:
+            print("Download já foi realizado")
 
 
 if __name__ == "__main__":
     print("CTRL+C para sair")
 
     filename = '2629_SP_2017_2.csv'
+    city = "Moji mirim"
+    state = "sp"
+    # date_hour = "2017-02-01 22:10:00"
 
-    open_file(filename)
+    file = ReadFile(filename, city, state, date_hour=None)
+    file.start_file()
