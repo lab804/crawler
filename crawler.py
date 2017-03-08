7b2ec3fb55a4f1c440629772b2e4d420e9ae2c0f
Murilo Ijanc <murilo@lab804.com.br>
2017-03-08 15:01:38 -0300

    primeiro commit

---
+++ crawler.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def acessa_site(url):
+    """acessa o site para gerar a sessao"""
+    pass
+
+def preenche_formulario(formulario):
+    """preenche formulario para receber o email"""
+    pass
+
+def ler_email(email, senha):
+    """ler os emails para capturar os que links que possuem
+    o dowload do arquivo csv"""
+    pass
+
+def download_arquivo(url):
+    """realiza o download do arquivo que contem
+    os dados metereologicos daquela estacao"""
+    pass
+
+def ler_arquivo(content):
+    """le o arquivo e checa se tem conteudo"""
+    pass
+
+def parsea_dados(content):
+    """limpamos os dados e estruturamos de uma forma
+    que se encaixe no banco de dados"""
+    pass
+
+def ultima_data():
+    """retorna dicionario com o ultimo documento inserido
+    no banco de dados"""
+    pass
+
+def inserir_dados(documento):
+    """insere o documento no banco de dados"""
+    pass
+
