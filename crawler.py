8de0ad3b8ecd3e4f675ee3781c73acfa69d43d9b
Murilo Ijanc <murilo@lab804.com.br>
2017-03-08 15:19:55 -0300

    acessando url e inicializando sessao

--- crawler.py
+++ crawler.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import requests
+
+# sessao global
+session = requests.Session()
+
 def acessa_site(url):
     """acessa o site para gerar a sessao"""
-    pass
+    req = session.get(url, timeout=10)
+    if req.status_code != 200:
+        print("Erro na conexao")
+        return None
+    else:
+        return req.content
+
 
 def preenche_formulario(formulario):
     """preenche formulario para receber o email"""
@@ -37,3 +48,13 @@ def inserir_dados(documento):
     """insere o documento no banco de dados"""
     pass
 
+def main():
+    """funcao principal"""
+    base_url = "http://150.163.255.234/salvar/mapainterativo/downpluv.php"
+    content = acessa_site(base_url)
+    if not content:
+        return
+    print(content)
+
+if __name__ == "__main__":
+    main()
