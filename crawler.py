30a48da3412a8c5fdc3dbaaa826c13658dd77e7f
barbara <barbara@lab804.com.br>
2017-03-16 11:24:42 -0300

    ISOdate implementado

--- crawler.py
+++ crawler.py
@@ -4,6 +4,7 @@ import requests
 import re, os
 import imaplib
 import getpass
+import dateutil.parser as parser
 
 # sessao global
 session = requests.Session()
@@ -127,7 +128,16 @@ def parsea_dados(keys, content):
             dic = {}
             for data in b_as_list:
                 if data:
-                    dic[keys[b_as_list.index(data)]] = data
+                    dec = data.decode()
+                    print(dec)
+                    print (b_as_list.index(data))
+                    if len(dec) == 21:
+                        date = (parser.parse(dec))
+                        print(date.isoformat())
+                        dh = date.isoformat()
+                        dic[keys[b_as_list.index(data)]] = dh
+                    else:
+                        dic[keys[b_as_list.index(data)]] = dec
             data_file.append(dic)
     if len(data_file) < 2:
         print ("Arquivo nao contem dados. Verifique se ha dados no site!")
@@ -154,48 +164,48 @@ def main():
     if not content:
         return
     else:
-        cities = organiza_dados(content, base_url)
-        for city in cities:
-            url_format = formata_url_path(base_url, city[0], city[1])
-            try:
-                iscaptcha = captura_captcha(url_img)
-                if iscaptcha:
-                    cap = input("Me fale as letras senhorita? ")
-                    print (cap)
-                    url_final = url_format+cap
-                    print(url_final)
-                    isget = preenche_formulario(url_final)
-                    if isget:
-                        salva_captcha_img(cap)
-                        salva_captcha_bd(cap)
+        # cities = organiza_dados(content, base_url)
+        # for city in cities:
+        #     url_format = formata_url_path(base_url, city[0], city[1])
+        #     try:
+        #         iscaptcha = captura_captcha(url_img)
+        #         if iscaptcha:
+        #             cap = input("Me fale as letras senhorita? ")
+        #             print (cap)
+        #             url_final = url_format+cap
+        #             print(url_final)
+        #             isget = preenche_formulario(url_final)
+        #             if isget:
+        #                 salva_captcha_img(cap)
+        #                 salva_captcha_bd(cap)
+        #         else:
+        #             print("Ops! nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
+        #     except requests.exceptions.HTTPError as e:
+        #             print("%s '%s'" % (e, url))
+        imap = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
+        imap_password = getpass.getpass("Enter your password --> ")
+        login_email(imap, imap_username, imap_password)
+        inbox = verifica_novo_email(imap)
+        if inbox != 0:
+            status, data = imap.search(None, FROM)
+            for num in reversed(data[0].split()):
+                data = ler_email(imap, link_re, num)
+                link_download = link_re.findall(data)
+                url_str= (b''.join(link_download).decode())
+                print(url_str)
+                isdownload = download_arquivo(url_str)
+                if isdownload:
+                    print("Sucesso no download do arquivo")
+                    imap.store(num, '+FLAGS', r'\Deleted')
+                    imap.expunge()
+                    print("Email excluido")
+                    isvalid = ler_arquivo(isdownload)
+                    if isvalid:
+                        parsea_dados(isvalid, isdownload)
                 else:
-                    print("Ops! nao foi possivel baixar o captcha, cheque a url ou se algo mudou no site.")
-            except requests.exceptions.HTTPError as e:
-                    print("%s '%s'" % (e, url))
-    imap = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
-    imap_password = getpass.getpass("Enter your password --> ")
-    login_email(imap, imap_username, imap_password)
-    inbox = verifica_novo_email(imap)
-    if inbox != 0:
-        status, data = imap.search(None, FROM)
-        for num in reversed(data[0].split()):
-            data = ler_email(imap, link_re, num)
-            link_download = link_re.findall(data)
-            url_str= (b''.join(link_download).decode())
-            print(url_str)
-            isdownload = download_arquivo(url_str)
-            if isdownload:
-                print("Sucesso no download do arquivo")
-                imap.store(num, '+FLAGS', r'\Deleted')
-                imap.expunge()
-                print("Email excluido")
-                isvalid = ler_arquivo(isdownload)
-                if isvalid:
-                    parsea_dados(isvalid, isdownload)
-            else:
-                print("Erro no download do arquivo, o email nao será excluido. Tente novamente.")
-    else:
-        print("Nao ha emails na caixa de entrada!")
+                    print("Erro no download do arquivo, o email nao será excluido. Tente novamente.")
+        else:
+            print("Nao ha emails na caixa de entrada!")
 
 if __name__ == "__main__":
     main()
